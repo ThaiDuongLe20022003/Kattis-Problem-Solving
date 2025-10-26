f9960e7b4e529df10ce8ca6353c5d9b167e620c9
duong <10421012@student.vgu.edu.vn>
2025-10-26 15:36:20 +0700

    join

---
+++ NUS Competitive Programming/joinstrings.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class joinstrings {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
+        
+        int n = Integer.parseInt(br.readLine());
+
+        var arr = new String[n];
+        for (int i = 0; i < n; i++) arr[i] = br.readLine();
+
+        var adjList = new ArrayList[n];
+        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<Integer>();
+
+        int start = 0;
+        for (int i = 0; i < n - 1; i++) {
+            var join = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            start = join[0] - 1;
+            adjList[start].add(join[1] - 1);
+        }
+
+        pw.print(arr[start]);
+        dfs(start, adjList, arr, pw);
+        pw.flush();
+    }
+
+    static void dfs(int v, ArrayList<Integer>[] adjList, String[] arr, PrintWriter pw) {
+        for (var u : adjList[v]) {
+            pw.print(arr[u]);
+            dfs(u, adjList, arr, pw);
+        }
+    }
+}
