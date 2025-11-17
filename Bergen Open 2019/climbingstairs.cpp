e4105223a2c5e3295ea44298c4f9cf92977843ad
duong <10421012@student.vgu.edu.vn>
2025-11-17 20:39:25 +0700

    climbing

---
+++ Bergen Open 2019/climbingstairs.cpp
@@ -0,0 +1,58 @@
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INP "solve"
+#define OUT "solve"
+
+const long long INF_LL = 8e18;
+const int INF = 1e9 + 100;
+const int MOD = 1e9 + 7;
+const long double EPS = 1e-10;
+const int BLOCK = 1000;
+const int dx[4] = {0, 0, 1, -1};
+const int dy[4] = {1, -1, 0, 0};
+mt19937 rnd(chrono::system_clock::now().time_since_epoch().count());
+
+void open_file() {
+    #ifdef THEMIS
+        freopen(INP ".inp","r",stdin);
+        freopen(OUT ".out","w",stdout);
+    #endif 
+}
+
+const int maxN = 1e6 + 100;
+
+void sol(){
+    int n, r, k;
+    cin >> n >> r >> k;
+    int ans = INF;
+    for (int x = 0; x <= 1000; x++) {
+        if (r + 2 * x >= n) {
+            int cost = max(2 * (r + x), 2 * k);
+            ans = min(ans, cost);
+        }
+    }
+    cout << ans;
+}
+
+void solve() {
+    clock_t begin = clock();
+    int T;
+    T = 1;
+    int TestCase = 0;
+    while (T--) {
+        TestCase += 1;
+        sol();
+    }
+    clock_t end = clock();
+    cerr << "Time: " << fixed << setprecision(10) << (double)(end - begin) / (double)CLOCKS_PER_SEC << '\n';
+}
+
+int main(int argc,char *argv[]) {
+    ios_base::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+    open_file();
+    solve();
+}
